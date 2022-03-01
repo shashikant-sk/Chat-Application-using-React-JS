08571dadeb4aba41bbe00d7635cf78b0213ff47b
shashikant <shashikantshah540@gmail.com>
2022-03-02 00:22:59 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
-const App = () => {
-    return (
-        <ChatEngine 
-        height="100vh"
-        projectID="f49f97e4-c8a1-468a-8241-251c758b8f20"
-        username="WILDKID"
-        userSecret="mom@143"
-        />
-    );
-}
 
-export default APP;
+const projectID = 'f49f97e4-c8a1-468a-8241-251c758b8f20';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="f49f97e4-c8a1-468a-8241-251c758b8f20"
+      userName="WILDKID"
+      userSecret="mom@143"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
