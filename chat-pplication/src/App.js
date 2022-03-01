50183ebe14d2927214ce72446b98782b62f194f3
shashikant <shashikantshah540@gmail.com>
2022-03-01 23:14:32 +0530

    push

--- chat-pplication/src/App.js
+++ chat-pplication/src/App.js
@@ -1,3 +1,16 @@
 import { ChatEngine } from 'react-chat-engine';
 
-import './app.css';
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+          height="100vh"
+          projectID="f49f97e4-c8a1-468a-8241-251c758b8f20"  
+          userName="WILDKID"
+          UserSecret="mom@143"
+        />
+    )
+}
+
+export default App;
